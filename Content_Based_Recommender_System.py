c4f3d995177130c171f5626011d0f83b6f6c9236
Guneet Singh <107395576+guneet-08@users.noreply.github.com>
2023-03-09 21:43:09 +0530

    Update Content_Based_Recommender_System.py

--- Content_Based_Recommender_System.py
+++ Content_Based_Recommender_System.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
+
+# A ML BASED ALGORITHM REPRESENTING THE FUNCTIONALITY OF CONTENT BASED FILTERING
+
 """
 Created on Sat Jan 7 15:10:21 2023
 
 @author: guneet
 """
 
+#importing the libraries to create a sample database
 import pandas as pd
 from random import randint
 
+#a function to generate the dataset based on various parameters
 def generate_data(n_books = 3000, n_genres = 10, n_authors = 450, n_publishers = 50, n_readers = 30000, dataset_size = 100000):
     '''
     This function will generate a dataset with features associated to
@@ -35,10 +40,9 @@ def generate_data(n_books = 3000, n_genres = 10, n_authors = 450, n_publishers =
         n_readers (Integer) : Number of readers for the dataset
         dataset_size (Integer) : The number of rows to be generated 
         
-    example:
-        data = generate_data()
     '''
     
+    #assigning randomized values to a dataframe based on parameters.
     d = pd.DataFrame(
         {
             'book_id' : [randint(1, n_books) for _ in range(dataset_size)],
@@ -52,16 +56,21 @@ def generate_data(n_books = 3000, n_genres = 10, n_authors = 450, n_publishers =
             'book_price' : [randint(1, 200) for _ in range(dataset_size)],
             'text_lang' : [randint(1,7) for _ in range(dataset_size)]
         }
-    ).drop_duplicates()
-    return d
-  
-d = generate_data(dataset_size = 100000)
-d.to_csv('data.csv', index = False)
+    ).drop_duplicates()         #dropping duplicates along the way (if generated)
+    return d                    #returung the data frame
 
 
+
+d = generate_data(dataset_size = 100000)            #genetaing a dataset using the above function (rows in DF: 100000)
+d.to_csv('data.csv', index = False)                 #converting the above generated dataset into a .CSV file
+
+
+#importing libraries for data preprocessing and normalization
 from numpy import dot
 from numpy.linalg import norm 
 
+
+#a function to normalize (scale) the generated data
 def normalize(data):
     '''
     This function will normalize the input data to be between 0 and 1
@@ -76,8 +85,10 @@ def normalize(data):
     if min_val < 0:
         data = [x + abs(min_val) for x in data]
     max_val = max(data)
-    return [x/max_val for x in data]
+    return [x/max_val for x in data]         #return a value between 0 and 1 for each element in the list
 
+
+#function to apply one hot encoding to specified column and update it in the DF
 def ohe(df, enc_col):
     '''
     This function will one hot encode the specified column and add it back
@@ -93,22 +104,29 @@ def ohe(df, enc_col):
     
     ohe_df = pd.get_dummies(df[enc_col])
     ohe_df.reset_index(drop = True, inplace = True)
-    return pd.concat([df, ohe_df], axis = 1)
+    return pd.concat([df, ohe_df], axis = 1)            #returns the new data frame with the encoded column
 
+
+#creating a class for the Content-Based Recommendation algorithm
 class CBRecommend():
+    
+    #takes the dataframe as an input
     def __init__(self, df):
         self.df = df
-        
+    
+    #funtion to calculate similarity between two vectors (dot product between them)
     def cosine_sim(self, v1,v2):
         '''
         This function will calculate the cosine similarity between two vectors
         '''
         return sum(dot(v1,v2)/(norm(v1)*norm(v2)))
     
+    
+    # a function to provide similar books based on the input book
     def recommend(self, book_id, n_rec):
         """
         df (dataframe): The dataframe
-        song_id (string): Representing the song name
+        book_id (string): Representing the book name
         n_rec (int): amount of rec user wants
         """
         
@@ -121,10 +139,18 @@ class CBRecommend():
 
 if __name__ == '__main__':
     # constants
-    PATH = '../data/data.csv'
+    PATH = '../data/data.csv'   # the location of the dataframe (or the csv file so created)
 
     # import data
-    df = pd.read_csv(PATH)
+    df = pd.read_csv(PATH)  #importing the dataset from the location 
+    
+    
+    """
+    NOTE :  In our case we have created the df already using the randomized function 
+            So, we'll be using the same below.
+    """
+    
+    df = d
 
     # normalize the num_pages, ratings, price columns
     df['num_pages_norm'] = normalize(df['num_pages'].values)
